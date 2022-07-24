df094243c00df4562fe797793c5eae87e05c10b7
Viktor Holovin <holovin@mail.com>
2022-07-24 10:09:06 +0200

    Create constructor for Combine class

--- src/ua/com/foxminded/helloworld/Combine.java
+++ src/ua/com/foxminded/helloworld/Combine.java
@@ -2,6 +2,11 @@ package ua.com.foxminded.helloworld;
 
 public class Combine extends AgriculturalMachinery {
 
+	public Combine(String name, int yearOfProduction) {
+		this.name = name;
+		this.yearOfProduction = yearOfProduction;
+	}
+
 	public boolean isReadyToService() {
 
 		if (this.getDistanceOnService() > 100000) {
